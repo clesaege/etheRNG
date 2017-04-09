29ec5ee0dbd9c80a261631d94d107027c58020d5
clesaege <clemage@live.fr>
2017-04-09 21:54:22 +0200

    Continued development

--- sequentialPoW/SequentialPoWRNG.sol
+++ sequentialPoW/SequentialPoWRNG.sol
@@ -12,6 +12,8 @@ contract SequentialPoWRNG {
     uint public jackpot; // Ether which would be given to those who manage to find the result before computeBlocks.
     uint public challengeTimePerStep=100; // The time to respond a challenge per answer.
     
+    address constant BURN=0x0; // Burned coins are sent to the address 0 to make it easy to know the amount of ETH burnt.
+    
     struct RN {
         bytes32 seed;
         uint difficulty;  // The amount of time the seed should be hashed.
@@ -28,19 +30,23 @@ contract SequentialPoWRNG {
         bytes32 result; // The result of applying repeated hash.
         uint deposit; // Deposit to be given back if the party has given the right value.
         Challenge[] challenges; // Challenges by people indicating that the result is false.
-        bool invalidated; // True if a challenger 
+        bool invalidated; // True if a challenger managed to win a challenge or the submitter has been timed out.
+        uint atStakeChallenge; // The sum of deposits in challenges of this submission. If this is 0, all challenges have been wiped out.
+        uint challengerReward; // The total amount claimable by the challengers who were not invalidated.
     }
     
     struct Challenge {
         address challenger;
-        bytes32 begin; // Firt value.
-        bytes32 end; // Last value according to the defender.
+        uint begin; // Where the challenged part starts.
+        uint end; // Where the challenged part ends.
+        bytes32 beginValue; // Firt value.
+        bytes32 endValue; // Last value according to the defender.
         bytes32 midValue; // Value in the middle according to the submitter. 0x0 before the submiter response.
-        uint distance; // Distance between begin and end.
         uint timeChallenger;
-        uint timeDefender;
+        uint timeSubmitter;
         uint lastInteractionTime; // Time of the last interaction.
         uint deposit; // Deposit to be given back if the party has challenged a false value.
+        bool invalidated; // True if the challenger failed his challenge or has been timed out.
     }
     
     RN[] public RNs;
@@ -88,32 +94,37 @@ contract SequentialPoWRNG {
      *  It is better to give the result directly if the right result has already been given.
      *  @param _idRN ID of the random number.
      *  @param _result Result of sequential PoW on the seed.
+     *  @return idSubmission The ID of the submission created.
      */
-    function publishResult(uint _idRN, bytes32 _result) payable cost(depositToFee*randomFee) {
+    function publishResult(uint _idRN, bytes32 _result) payable cost(depositToFee*randomFee) returns(uint idSubmission) {
         RN rn = RNs[_idRN];
         
-        assert(rn.startTime+computeMaxTime>now); // Verify we can still submit.
-        
-        Submission submission=rn.submissions[rn.submissions.length++];
+        require(rn.startTime+computeMaxTime>now); // Verify we can still submit.
+        idSubmission=rn.submissions.length++;
+        Submission submission=rn.submissions[idSubmission];
         submission.submitter=msg.sender;
         submission.result=_result;
         submission.deposit=msg.value;
+        
+        return idSubmission;
     }
     
     /** Give a commitment of the result. 
      *  @param _idRN ID of the random number.
      *  @param _commitment The hash of the result and the address of the sender.
+     *  @return idSubmission The ID of the submission created.
      */
-    function commitResult(uint _idRN, bytes32 _commitment) payable cost(depositToFee*randomFee) {
+    function commitResult(uint _idRN, bytes32 _commitment) payable cost(depositToFee*randomFee) returns(uint idSubmission) {
         RN rn = RNs[_idRN];
         
-        assert(rn.startTime+computeMaxTime>now); // Verify submissions are still open.
-        
-        Submission submission=rn.submissions[rn.submissions.length++];
+        require(rn.startTime+computeMaxTime>now); // Verify submissions are still open.
+        idSubmission=rn.submissions.length++;
+        Submission submission=rn.submissions[idSubmission];
         submission.submitter=msg.sender;
         submission.commitment=_commitment;
         submission.deposit=msg.value;
         
+        return idSubmission;
     }
     
     /** Reveal your commmitment.
@@ -125,8 +136,8 @@ contract SequentialPoWRNG {
         RN rn = RNs[_idRN];
         Submission submission=rn.submissions[_idSubmission];
         
-        assert(rn.startTime+computeMaxTime>now); // Verify submissions are still open.
-        assert(keccak256(_result,msg.sender)==submission.commitment); // Verify the revealed result match the commitment. Note that this also prevent someone from revealing challenges of other accounts.
+        require(rn.startTime+computeMaxTime>now); // Verify submissions are still open.
+        require(keccak256(_result,msg.sender)==submission.commitment); // Verify the revealed result match the commitment. Note that this also prevent someone from revealing challenges of other accounts.
         
         submission.result=_result;
     }
@@ -136,34 +147,193 @@ contract SequentialPoWRNG {
      *  @param _idRN ID of the random number.
      *  @param _idSubmission ID of the submission.
      *  @param _t Time multiplier for the difficulty, use function timeMultiplier to compute it offchain.
+     *  @return idChallenge The ID of the challenge created.
      */
-    function challenge(uint _idRN, uint _idSubmission, uint _t) payable cost(depositToFee*randomFee) {
+    function challenge(uint _idRN, uint _idSubmission, uint _t) payable cost(depositToFee*randomFee) returns (uint idChallenge) {
         RN rn = RNs[_idRN];
         Submission submission=rn.submissions[_idSubmission];
-        assert(rn.startTime+challengeTime>now); // Verify challenges are still open.
-        assert(2**(_t-1) <= rn.difficulty && 2**_t > rn.difficulty); // Note that underflow are not an issue since 2**0=1>rn.difficulty is false, nor are oveflows since oveflowedValue=0>rn.difficulty is also false.
+        require(rn.startTime+challengeTime>now); // Verify challenges are still open.
+        require(2**(_t-1) <= rn.difficulty && 2**_t > rn.difficulty); // Note that underflow are not an issue since 2**0=1>rn.difficulty is false, nor are oveflows since oveflowedValue=0>rn.difficulty is also false.
+        require(!submission.invalidated);
         
-        // Create the challenge
-        Challenge challenge=submission.challenges[submission.challenges.length++];
+        // Create the challenge.
+        idChallenge=submission.challenges.length++;
+        Challenge challenge=submission.challenges[idChallenge];
         challenge.challenger=msg.sender;
-        challenge.begin=rn.seed;
-        challenge.end=submission.result;
-        challenge.distance=rn.difficulty;
+        challenge.beginValue=rn.seed;
+        challenge.endValue=submission.result;
+        challenge.begin=0;
+        challenge.end=rn.difficulty;
         challenge.timeChallenger=challengeTimePerStep*_t;
-        challenge.timeDefender=challenge.timeChallenger;
+        challenge.timeSubmitter=challengeTimePerStep*_t;
         challenge.lastInteractionTime=now;
         challenge.deposit=msg.value;
-    }
-    
-    /** 
-     *  @param _midValue Value of hash^(difficulty/2)(seed).
-     */
-    function respondChallenge(bytes32 _midValue) {
         
+        submission.atStakeChallenge+=challenge.deposit;// Increment the number of active challenge for the submission.
+        
+        return idChallenge;
     }
-
     
-    // Constant functions.
+    
+    /** Respond to a challenge by giving the medium value.
+     *  This function should be called by the submitter.
+     *  @param _idRN ID of the random number.
+     *  @param _idSubmission ID of the submission.
+     *  @param _idChallenge ID of the challenge.
+     *  @param _midValue Value of hash^(distance/2)(begin).
+     */
+    function respondChallenge(uint _idRN, uint _idSubmission, uint _idChallenge, bytes32 _midValue) {
+        RN rn = RNs[_idRN];
+        Submission submission=rn.submissions[_idSubmission];
+        Challenge challenge=submission.challenges[_idChallenge];
+        
+        require(!submission.invalidated);
+        require(!challenge.invalidated);
+        require(submission.submitter==msg.sender); // Verify that it's called by the submitter.
+        require(challenge.midValue==0x0); // Verify the value has not been given yet (to avoid transaction ordering attack).
+        require(now-challenge.lastInteractionTime>=challenge.timeSubmitter); // Verify that submitter still have time.
+        
+        challenge.midValue=_midValue; // Set the midValue.
+        challenge.timeSubmitter-=(now-challenge.lastInteractionTime); // Update the time left by for submitter. 
+        challenge.lastInteractionTime=now;
+    }
+    
+    
+    /** Continue the challenge in a reduced search space according to the answer of submitter.
+     *  This function should be called by the challenger.
+     *  @param _idRN ID of the random number.
+     *  @param _idSubmission ID of the submission.
+     *  @param _idChallenge ID of the challenge.
+     *  @param _midValueOK True if the submitter gave the right value of hash^(distance/2)(begin), false otherwise.
+     */
+    function continueChallenge(uint _idRN, uint _idSubmission, uint _idChallenge, bool _midValueOK) {
+        RN rn = RNs[_idRN];
+        Submission submission=rn.submissions[_idSubmission];
+        Challenge challenge=submission.challenges[_idChallenge];
+        
+        require(!submission.invalidated);
+        require(!challenge.invalidated);
+        require(challenge.challenger==msg.sender); // Verify that it's called by the challenger.
+        require(challenge.midValue!=0x0); // Verify that a midValue has been given by the submitter.
+        require(now-challenge.lastInteractionTime>=challenge.timeChallenger); // Verify that the challenger still have time.
+        
+        
+        if (_midValueOK) { // The submitter gave the correct midValue, his means his error is in the second half.
+            challenge.beginValue=challenge.midValue; 
+            challenge.begin=(challenge.end-challenge.begin)/2;
+        }
+        else { // The submitter gave a incorrect midValue, this means his error is in the first half.
+            challenge.endValue=challenge.midValue;
+            challenge.end=(challenge.end-challenge.begin)/2;
+        }
+        
+        delete challenge.midValue; // Clean the midValue. The submitter will have to give the midValue in the reduced search space.
+        challenge.timeChallenger-=(now-challenge.lastInteractionTime); // Update the time left for the challenger.
+        challenge.lastInteractionTime=now;
+        
+        assert(challenge.begin!=challenge.end); // Make sure not to reduce the space to a single point. When the distance is only one user need to call endChallenge.
+    }
+    
+        /** Invalidate a submission.
+     *  One fourth of the deposit is transfered to the jackpot.
+     *  One fourth is burned.
+     *  The other half can be claimed by the challengers in proportion of their deposit.
+     *  @param _submission The submission which was invalidated.
+     */
+    function invalidateSubmission(Submission _submission) internal {
+        require(!_submission.invalidated);
+        
+        _submission.invalidated=true; // Set the submission as invalid.
+        
+        jackpot+=_submission.deposit/4;
+        BURN.transfer(_submission.deposit/4);
+        _submission.challengerReward=_submission.deposit/2;
+        _submission.deposit=0;
+    }
+    
+    /** Invalidate a challenge.
+     *  Transfer half of the challenger deposit to the submitter deposit.
+     *  Transfer one fourth to the jackpot.
+     *  The remaining is burned.
+     *  This ensure that even if one party is always winning the jackopt, it can't make false challenges for free.
+     *  @param _submission The submission the challenge was challenging.
+     *  @param _challenge The challenge which is invalidate.
+     */
+    function invalidateChallenge(Submission _submission, Challenge _challenge) internal {
+        require(!_challenge.invalidated);
+        
+        _challenge.invalidated=true; // Set the challenge as invalid.
+        _submission.atStakeChallenge-=_challenge.deposit;
+        
+        // Transfert the deposit to the submission and the jackpot.
+        // Note that a wei might be lost due to rounding but it is not an issue.
+        _submission.deposit+=(_challenge.deposit/2);
+        jackpot+=(_challenge.deposit/4); 
+        BURN.transfer(_challenge.deposit/4);
+        _challenge.deposit=0;
+    }
+    
+    /** Finish a challenge, if the challenge is successfull, invalidate the submission, else invalidate the challenge.
+     *  Anyone can call this function when the search space is reduced to two values.
+     *  @param _idRN ID of the random number.
+     *  @param _idSubmission ID of the submission.
+     *  @param _idChallenge ID of the challenge.
+     */
+    function endChallenge(uint _idRN, uint _idSubmission, uint _idChallenge) {
+        RN rn = RNs[_idRN];
+        Submission submission=rn.submissions[_idSubmission];
+        Challenge challenge=submission.challenges[_idChallenge];
+        
+        require(!submission.invalidated);
+        require(!challenge.invalidated);
+        require((challenge.end-challenge.begin)==1); // We can only call this function when there is only two values remaing in the search space.
+        
+        if(keccak256(challenge.beginValue)==challenge.endValue)
+            invalidateChallenge(submission,challenge); // The submitter passed the challenge, so the challenger failed.
+        else // The challenger won his challenge, so the submitter failed.
+            invalidateSubmission(submission);
+    }
+    
+    
+    /** Invalidate a submission due to the submitter timing out in one challenge.
+     *  @param _idRN ID of the random number.
+     *  @param _idSubmission ID of the submission.
+     *  @param _idChallenge ID of the challenge the submitter failed to respond.
+     */
+    function timeOutSubmitter(uint _idRN, uint _idSubmission, uint _idChallenge) {
+        RN rn = RNs[_idRN];
+        Submission submission=rn.submissions[_idSubmission];
+        Challenge challenge=submission.challenges[_idChallenge];
+        
+        require(!submission.invalidated);
+        require(!challenge.invalidated);
+        require(challenge.midValue==0); // It is the turn of the submitter to respond.
+        require(now-challenge.lastInteractionTime>challenge.timeSubmitter);
+        
+        invalidateSubmission(submission);
+    }
+    
+    /** Time out a challenger.
+     *  @param _idRN ID of the random number.
+     *  @param _idSubmission ID of the submission.
+     *  @param _idChallenge ID of the challenge the submitter failed to respond.
+     */
+    function timeOutChallenger(uint _idRN, uint _idSubmission, uint _idChallenge) {
+        RN rn = RNs[_idRN];
+        Submission submission=rn.submissions[_idSubmission];
+        Challenge challenge=submission.challenges[_idChallenge];
+        
+        require(!challenge.invalidated);
+        require(!submission.invalidated);
+        
+        require(challenge.midValue!=0); // It is the turn of the challenger to respond.
+        require(now-challenge.lastInteractionTime>challenge.timeChallenger);
+        
+        invalidateChallenge(submission,challenge);
+    }
+    
+    
+    // Constant functions 
     
     /** Return your personal commitment of a result.
      *  @param _result The result to commit.
